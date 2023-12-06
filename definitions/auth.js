3f3232cc8cae0df98fe3108b133963a62d9d2982
Louis Bertson <louisbertson@gmail.com>
2023-12-06 11:11:25 +0000

    fixed authorization

--- definitions/auth.js
+++ definitions/auth.js
@@ -1,10 +1,13 @@
 var user = { id: 'IZ1bLY1cm61f', name: 'John Doe', sa: true };
 AUTH(function($) {
-    // vary simple authorization middleware
+    // very simple authorization middleware
     var cart = $.cookie(CONF.cart_cookie); // get cart id from client cookie
 
-    if (cart)
-        user.cartid = cart;
+    if (!cart) {
+        $.invalid();
+        return;
+    }
 
+    user.cartid = cart;
     $.success(user); 
 });
